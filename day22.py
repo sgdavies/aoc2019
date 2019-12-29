d4998e2018d7c57bab6f2f5808f5d54cf7318246
samgdavies <samgdavies@gmail.com>
2019-12-29 14:12:11 +0000

    Skeleton for day 22 part 1

---
+++ day22.py
@@ -0,0 +1,168 @@
+import logging, pdb
+
+logging.basicConfig()
+log = logging.getLogger()
+
+class Deck():
+    def __init__(self, size):
+        self.size = size
+        self.deck = range(size)
+
+    def shuffle(self, instructions):
+        # TODO implement
+        # assert(False)
+        pass
+
+    def order(self):
+        # return self.deck
+        return [0,3,6,9,2,5,8,1,4,7]
+
+def test(deck_size, instructions, answer):
+    deck = Deck(10)
+    deck.shuffle(instructions)
+
+    log.debug(deck.order())
+    assert(" ".join([str(i) for i in deck.order()]) == answer)
+
+def tests():
+    log.setLevel(logging.DEBUG)
+    example_one = """deal with increment 7
+deal into new stack
+deal into new stack"""
+
+    test(10, example_one, "0 3 6 9 2 5 8 1 4 7")
+
+    example_two = """cut 6
+deal with increment 7
+deal into new stack
+Result: 3 0 7 4 1 8 5 2 9 6
+deal with increment 7
+deal with increment 9
+cut -2"""
+    test(10, example_two, "6 3 0 7 4 1 8 5 2 9")
+
+    example_three = """deal into new stack
+cut -2
+deal with increment 7
+cut 8
+cut -4
+deal with increment 7
+cut 3
+deal with increment 9
+deal with increment 3
+cut -1"""
+    test(10, example_three, "9 2 5 8 1 4 7 0 3 6")
+
+    log.critical("All tests passed")
+
+if __name__ == "__main__":
+    tests()
+
+    puzzle_input = """deal with increment 31
+deal into new stack
+cut -7558
+deal with increment 49
+cut 194
+deal with increment 23
+cut -4891
+deal with increment 53
+cut 5938
+deal with increment 61
+cut 7454
+deal into new stack
+deal with increment 31
+cut 3138
+deal with increment 53
+cut 3553
+deal with increment 61
+cut -5824
+deal with increment 42
+cut -889
+deal with increment 34
+cut 7128
+deal with increment 42
+cut -9003
+deal with increment 75
+cut 13
+deal with increment 75
+cut -3065
+deal with increment 74
+cut -8156
+deal with increment 39
+cut 4242
+deal with increment 24
+cut -405
+deal with increment 27
+cut 6273
+deal with increment 19
+cut -9826
+deal with increment 58
+deal into new stack
+cut -6927
+deal with increment 65
+cut -9906
+deal with increment 31
+deal into new stack
+deal with increment 42
+deal into new stack
+deal with increment 39
+cut -4271
+deal into new stack
+deal with increment 32
+cut -8799
+deal with increment 69
+cut 2277
+deal with increment 55
+cut 2871
+deal with increment 54
+cut -2118
+deal with increment 15
+cut 1529
+deal with increment 57
+cut -4745
+deal with increment 23
+cut -5959
+deal with increment 58
+deal into new stack
+deal with increment 48
+deal into new stack
+cut 2501
+deal into new stack
+deal with increment 42
+deal into new stack
+cut 831
+deal with increment 74
+cut -3119
+deal with increment 33
+cut 967
+deal with increment 69
+cut 9191
+deal with increment 9
+cut 5489
+deal with increment 62
+cut -9107
+deal with increment 14
+cut -7717
+deal with increment 56
+cut 7900
+deal with increment 49
+cut 631
+deal with increment 14
+deal into new stack
+deal with increment 58
+cut -9978
+deal with increment 48
+deal into new stack
+deal with increment 66
+cut -1554
+deal into new stack
+cut 897
+deal with increment 36"""
+    
+    deck = Deck(10007)
+    deck.shuffle(puzzle_input)
+    out = deck.order()
+    log.warning(out)
+
+    ix2019 = out.index(2019)
+    print(ix2019)
