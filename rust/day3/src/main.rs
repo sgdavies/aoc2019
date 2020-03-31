15d3e3775b7b1a316601bbba3f392e20acdf44c8
Sam Davies <sam.davies@metaswitch.com>
2020-03-31 19:43:02 +0100

    Day 3 - structs, enums, impls

--- rust/day3/src/main.rs
+++ rust/day3/src/main.rs
@@ -1,9 +1,67 @@
+#[derive(Debug)]
+enum Direction {
+    U, D, L, R,
+}
+
+struct SegmentDefinition(Direction, i32);
+struct Extents {
+    minx: i32, maxx: i32, miny: i32, maxy: i32,
+}
+
+struct Wire {
+    segments: Vec<SegmentDefinition>,
+    extents: Extents,
+}
+
+impl Wire {
+    fn from_string(description: &str) -> Wire {
+        let mut vec = Vec::new();
+        for segment in description.split(',') {
+            let (dir, dist) = (&segment[0..1], segment[1..].parse::<i32>().unwrap());
+            let dir = match dir {
+                "U" => Direction::U,
+                "D" => Direction::D,
+                "L" => Direction::L,
+                "R" => Direction::R,
+                _ => panic!("Unsupported direction: {}", dir),
+            };
+            vec.push(SegmentDefinition(dir, dist));
+        }
+
+        let extents = Wire::find_extents(&vec);
+        Wire{ segments: vec, extents: extents, }
+    }
+
+    fn find_extents(segments: &Vec<SegmentDefinition>) -> Extents {
+        // TODO implement
+        Extents{minx: 0, maxx: 0, miny: 0, maxy: 0}
+    }
+}
+
 fn main() {
     run_tests();
 }
 
 fn get_distance(wire_one: &str, wire_two: &str) -> i32 {
+    let wire_one = Wire::from_string(wire_one);
+    let wire_two = Wire::from_string(wire_two);
     6
+}
+
+fn wire_from_string(wire: &str) -> Vec<SegmentDefinition> {
+    let mut vec = Vec::new();
+    for segment in wire.split(',') {
+        let (dir, dist) = (&segment[0..1], segment[1..].parse::<i32>().unwrap());
+        let dir = match dir {
+            "U" => Direction::U,
+            "D" => Direction::D,
+            "L" => Direction::L,
+            "R" => Direction::R,
+            _ => panic!("Unsupported direction: {}", dir),
+        };
+        vec.push(SegmentDefinition(dir, dist));
+    }
+    vec
 }
 
 fn run_tests() {
