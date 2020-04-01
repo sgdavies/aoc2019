f18b5e08f5084b327558b0191540405eee8da78e
Sam Davies <sam.davies@metaswitch.com>
2020-04-01 20:46:28 +0100

    Day 3 - nearly done - maps etc

--- rust/day3/src/main.rs
+++ rust/day3/src/main.rs
@@ -1,16 +1,29 @@
+use std::collections::HashMap;
+
 #[derive(Debug)]
 enum Direction {
     U, D, L, R,
 }
 
-struct SegmentDefinition(Direction, i32);
-struct Extents {
-    minx: i32, maxx: i32, miny: i32, maxy: i32,
+struct SegmentDefinition {
+    direction: Direction, 
+    distance: i32,
+}
+
+#[derive(PartialEq, Eq, Hash, Clone, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Point {
+    fn manhatten_distance(&self, other: &Point) -> i32 {
+        (self.x - other.x).abs() + (self.y - other.y).abs()
+    }
 }
 
 struct Wire {
     segments: Vec<SegmentDefinition>,
-    extents: Extents,
 }
 
 impl Wire {
@@ -25,16 +38,33 @@ impl Wire {
                 "R" => Direction::R,
                 _ => panic!("Unsupported direction: {}", dir),
             };
-            vec.push(SegmentDefinition(dir, dist));
+            vec.push(SegmentDefinition { direction: dir, distance: dist, });
         }
 
-        let extents = Wire::find_extents(&vec);
-        Wire{ segments: vec, extents: extents, }
+        Wire{ segments: vec, }
     }
 
-    fn find_extents(segments: &Vec<SegmentDefinition>) -> Extents {
-        // TODO implement
-        Extents{minx: 0, maxx: 0, miny: 0, maxy: 0}
+    fn trace_path(&self, circuit_board: &mut HashMap<Point,u32>) {
+        // Don't insert the origin
+        let mut location = Point { x: 0, y: 0, };
+
+        for segment in &self.segments {
+            self.trace_segment(&segment, &mut location, circuit_board);
+        }
+    }
+
+    fn trace_segment(&self, segment: &SegmentDefinition, location: &mut Point, circuit_board: &mut HashMap<Point,u32>) {
+        for _ in 0..segment.distance {
+            match segment.direction {
+                Direction::U => location.y += 1,
+                Direction::D => location.y -= 1,
+                Direction::L => location.x += 1,
+                Direction::R => location.x -= 1,
+            }
+
+            let visited = circuit_board.entry(location.clone()).or_insert(0);
+            *visited += 1;
+        }
     }
 }
 
@@ -45,23 +75,18 @@ fn main() {
 fn get_distance(wire_one: &str, wire_two: &str) -> i32 {
     let wire_one = Wire::from_string(wire_one);
     let wire_two = Wire::from_string(wire_two);
-    6
-}
 
-fn wire_from_string(wire: &str) -> Vec<SegmentDefinition> {
-    let mut vec = Vec::new();
-    for segment in wire.split(',') {
-        let (dir, dist) = (&segment[0..1], segment[1..].parse::<i32>().unwrap());
-        let dir = match dir {
-            "U" => Direction::U,
-            "D" => Direction::D,
-            "L" => Direction::L,
-            "R" => Direction::R,
-            _ => panic!("Unsupported direction: {}", dir),
-        };
-        vec.push(SegmentDefinition(dir, dist));
-    }
-    vec
+    let mut circuit_board: HashMap<Point,u32> = HashMap::new();
+
+    wire_one.trace_path(&mut circuit_board);
+    wire_two.trace_path(&mut circuit_board);
+
+    for (k, v) in circuit_board.iter()
+        .filter(|&(k, v)| *v > 1) {
+            // Map to manhatten distances, then sort, then print closest distance
+            println!("{:?} = {}", k, v);
+        }
+    6 // replace with actual value
 }
 
 fn run_tests() {
