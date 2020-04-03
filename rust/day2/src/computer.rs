a50f8ca86d6a81465710bcb17624ff5867b8bba8
Sam Davies <sam.davies@metaswitch.com>
2020-04-03 19:33:56 +0100

    WIP: opcodes, input, output - but not working (day 5 code in day 2)

--- rust/day2/src/computer.rs
+++ rust/day2/src/computer.rs
@@ -2,6 +2,8 @@ pub mod computer {
     pub struct Computer {
         ip: usize, // instruction pointer
         memory: Vec<i32>,
+        inputs: Vec<i32>,
+        outputs: Vec<i32>,
     }
     
     impl Computer {
@@ -9,6 +11,8 @@ pub mod computer {
             Computer {
                 ip : 0,
                 memory : program,
+                inputs: Vec::new(),
+                outputs: Vec::new(),
             }
         }
     
@@ -18,8 +22,16 @@ pub mod computer {
                 let instruction = Instruction::new(&self.memory[self.ip]);
                 match instruction.opcode {
                     1 => { // add
-                        let a = self.value_at(*&self.memory[self.ip+1] as usize);
-                        let b = self.value_at(*&self.memory[self.ip+2] as usize);
+                        let a_addr = match instruction.modes[0] {
+                            Mode::Immediate => self.ip+1, // immediate mode
+                            Mode::Position => self.memory[self.ip+1] as usize,
+                        };
+                        let b_addr = match instruction.modes[1] {
+                            Mode::Immediate => self.ip+2, // immediate mode
+                            Mode::Position => self.memory[self.ip+2] as usize,
+                        };
+                        let a = self.value_at(a_addr);
+                        let b = self.value_at(b_addr);
                         let store_loc = *&self.memory[self.ip+3] as usize;
                         let val = a + b;
                         self.memory[store_loc] = val;
@@ -34,6 +46,22 @@ pub mod computer {
                         self.memory[store_loc] = val;
                         self.ip += 4;
                     },
+
+                    3 => { // read input
+                        let addr = match instruction.modes[0] {
+                            Mode::Immediate => self.ip+1, // immediate mode
+                            Mode::Position => self.memory[self.ip+1] as usize,
+                        };
+                        self.memory[addr] = self.inputs.remove(0);
+                        self.ip += 2;
+                    },
+
+                    4 => { // append output
+                        let val = self.memory[self.ip+1];
+                        println!("Output: {}", &val);
+                        self.outputs.push(val);
+                        self.ip += 2;
+                    }
     
                     99 => break, // exit
     
@@ -45,11 +73,25 @@ pub mod computer {
         pub fn value_at(&self, location: usize) -> i32 {
             self.memory[location]
         }
+
+        pub fn add_input(&mut self, input: i32) {
+            self.inputs.push(input);
+        }
+
+        pub fn get_next_output(&mut self) -> i32 {
+            self.outputs.remove(0)
+        }
+    }
+
+    #[derive(Debug)]
+    enum Mode {
+        Immediate,
+        Position,
     }
 
     struct Instruction {
         opcode: i32,
-        modes: Vec<bool>,
+        modes: Vec<Mode>,
     }
 
     impl Instruction {
@@ -58,15 +100,21 @@ pub mod computer {
             let n_modes = match opcode {
                 1 => 3,
                 2 => 3,
+                3 => 1,
                 _ => 0,
             };
             Instruction { modes: Instruction::get_modes(modes, n_modes), opcode: opcode, }
         }
 
-        fn get_modes(modes: i32, n: i32) -> Vec<bool> {
-            let mut v: Vec<bool> = Vec::new();
+        fn get_modes(modes: i32, n: i32) -> Vec<Mode> {
+            let mut v: Vec<Mode> = Vec::new();
             for p in 0..n as u32 {
-                v.push( (modes / 10_i32.pow(p)) % (10_i32.pow(p+1)) == 1 );
+                let mode: i32 = (modes / 10_i32.pow(p)) % (10_i32.pow(p+1));
+                v.push( match mode {
+                    0 => Mode::Position,
+                    1 => Mode::Immediate,
+                    _ => panic!("Unsupported mode! {}", mode)
+                } );
             }
             v
         }
